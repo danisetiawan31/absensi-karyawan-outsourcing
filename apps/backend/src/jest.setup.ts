e96cd5055c6a6b7e3ede2381cc8e150065cd9b69
Ahmad Dhani Setiawan <dnistwn31@gmail.com>
2026-09-08 18:54:04 +0700

    fix(ci): inject environment variables for turborepo and jest test runner

--- apps/backend/src/jest.setup.ts
+++ apps/backend/src/jest.setup.ts
@@ -1,1 +1,7 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+// Load .env dari apps/backend/.env jika ada
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 process.env.TZ = 'Asia/Jakarta';
